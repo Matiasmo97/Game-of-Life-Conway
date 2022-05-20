4a0d1c5553f2eecbc77a8aa884c4e34a6e04d937
Matias Montini <91911135+Matiasmo97@users.noreply.github.com>
2022-05-20 17:33:14 -0300

    commit

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,57 +9,74 @@ function Game() {
   let [play, setPlay] = useState(false);
   let [iniciar, setIniciar] = useState(false);
   let [active, setActive] = useState(false);
-  let [resetState, setResetState] = useState(false);
+  let [resetState] = useState(false);
   let [time, setTime] = useState(3);
   let [grilla, setGrilla] = useState("");
   let [rows, setRows] = useState(30);
   let [columns, setColumns] = useState(50);
-  let [board, setBoard] = useState([]);
+  let [pause, setPause] = useState(false);
+  let board;
 
   let initialBoard = new Array(30).fill(0).map(() => new Array(50).fill(0));
-  
-    
-
 
   useEffect(() => {
     let interval = null;
     if (play) {
-      initial()
+      initial();
     }
 
     if (active) {
-      interval = setInterval(() => {
-        drawBoard(board);
-        setCells(board);
-        
-        setGeneration((generation) => ++generation);
-      }, time * 1000);
+      if (pause) {
+        interval = setInterval(() => {
+          drawBoard(cells);
+          setGeneration((generation) => ++generation);
+        }, time * 1000);
+      } else {
+        interval = setInterval(() => {
+          drawBoard(board);
+          setCells(board);
+          setGeneration((generation) => ++generation);
+        }, time * 1000);
+      }
     }
 
-
-    if (resetState) {
-      reset();
-      clearInterval(interval);
-    }
     return () => clearInterval(interval);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [active, resetState, play]);
+  }, [active, resetState, play, pause]);
+
+  //Funcion que inicializa el tablero de celulas
+  async function initial() {
+    //Creamos el tablero
+    board = newCells(rows, columns);
+
+    //Iniciamos el tablero
+    await startGame(board);
+  }
 
   //Función para iniciar/detener el juego
   function toggle() {
     setActive(!active);
-    setPlay(!play)
+    setPlay(true);
+    setPause(true);
   }
   function toggle2() {
     setActive(!active);
     //Cambiamos el stado de Ready
     setReady(false);
 
-    setIniciar(true)
+    setIniciar(true);
 
-    setPlay(true)
+    setPlay(true);
   }
- 
+  //Función para reiniciar el tablero de células
+  function reset() {
+    setGeneration(0);
+    setReady(true);
+    setActive(false);
+    board = [];
+    setCells([]);
+    setIniciar(false);
+  }
 
   //Creamos un useEffect para actualizar el tiempo,
   // dependiendo de lo que elija el usuario
@@ -96,101 +113,84 @@ function Game() {
     }
   }, [grilla]);
 
-  //Funcion que inicializa el tablero de celulas
-  async function initial() {
-    //Creamos el tablero
-    board = newCells(rows, columns);
-
-    //Iniciamos el tablero
-    await startGame(board);
-  }
-
   //====== CÉLULAS ======
   // Objeto de Células(Le pasamos por parametro sus condenadas, y su estado)
-  const Cells = function (x, y, state) {
-    //Cordenadas
-    this.x = x;
-    this.y = y;
-    this.id = uuidv4();
+  class Cells {
+    constructor(x, y, state) {
+      //Cordenadas
+      this.x = x;
+      this.y = y;
+      this.id = uuidv4();
 
-    //Condición(vivo o muerto)
-    this.condition = state; //vivo = 1, muerto = 0
-    this.conditionProx = this.condition;
-
-    //Creamos un array para guardar los vecinos
-    this.neighbours = [];
-
-    //Creamos una función que añada los vecinos
-    this.addNeighbours = function () {
-      let neighboursX;
-      let neighboursY;
-
-      //Creamos el dos bucles for para identificar los vecinos de mi célula
-      for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-          neighboursX = (this.x + j + columns) % columns;
-          neighboursY = (this.y + i + rows) % rows;
-
-          //Descartamos la célula actual
-          if (i !== 0 || j !== 0) {
-            this.neighbours.push(board[neighboursY][neighboursX]);
-          }
-        }
-      }
-    };
-
-    //Las leyes de Conway
-    this.newCycle = function () {
-      let allNeighbours = 0;
-
-      //Recorremos el array de vecinos para sumarlos y saber cuantos tiene la célula
-      for (let i = 0; i < this.neighbours.length; i++) {
-        allNeighbours += this.neighbours[i].condition;
-      }
-
-      //====== APLICAMOS LAS LEYES DE CONWAY ======
-      //Por defecto va a tener el estado anterior
+      //Condición(vivo o muerto)
+      this.condition = state; //vivo = 1, muerto = 0
       this.conditionProx = this.condition;
 
-      //Si tiene menos de 2 o más de 3 muere
-      if (allNeighbours < 2 || allNeighbours > 3) {
-        this.conditionProx = 0;
-      }
-      //Si tiene 3 vecinos nacé o se mantiene vivo
-      if (allNeighbours === 3) {
-        this.conditionProx = 1;
-      }
-    };
+      //Creamos un array para guardar los vecinos
+      this.neighbours = [];
 
-    this.mutation = function () {
-      this.condition = this.conditionProx;
-    };
-  };
+      //Creamos una función que añada los vecinos
+      this.addNeighbours = function () {
+        let neighboursX;
+        let neighboursY;
 
-  // ====== FUNCIONES DEL JUEGO ======
-  //Función para reiniciar el tablero de células
-  function reset() {
-    setGeneration(0);
-    setReady(true);
-    setActive(false);
-    board = [];
-    setCells([]);
-    setIniciar(false)
+        //Creamos el dos bucles for para identificar los vecinos de mi célula
+        for (let i = -1; i < 2; i++) {
+          for (let j = -1; j < 2; j++) {
+            neighboursX = (this.x + j + columns) % columns;
+            neighboursY = (this.y + i + rows) % rows;
+
+            //Descartamos la célula actual
+            if (i !== 0 || j !== 0) {
+              this.neighbours.push(board[neighboursY][neighboursX]);
+            }
+          }
+        }
+      };
+
+      //Las leyes de Conway
+      this.newCycle = function () {
+        let allNeighbours = 0;
+
+        //Recorremos el array de vecinos para sumarlos y saber cuantos tiene la célula
+        for (let i = 0; i < this.neighbours.length; i++) {
+          allNeighbours += this.neighbours[i].condition;
+        }
+
+        //====== APLICAMOS LAS LEYES DE CONWAY ======
+        //Por defecto va a tener el estado anterior
+        this.conditionProx = this.condition;
+
+        //Si tiene menos de 2 o más de 3 muere
+        if (allNeighbours < 2 || allNeighbours > 3) {
+          this.conditionProx = 0;
+        }
+        //Si tiene 3 vecinos nacé o se mantiene vivo
+        if (allNeighbours === 3) {
+          this.conditionProx = 1;
+        }
+      };
+
+      this.mutation = function () {
+        this.condition = this.conditionProx;
+      };
+    }
   }
 
+  // ====== FUNCIONES DEL JUEGO ======
   //Función que calcula los proximos estados
-  function drawBoard(board) {
+  function drawBoard(obj) {
     //Calculamos el siguiente ciclo
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < columns; x++) {
-        board[y][x].newCycle();
+        obj[y][x].newCycle();
       }
     }
 
     //Aplicamos la mutacion
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < columns; x++) {
-        board[y][x].mutation();
+        obj[y][x].mutation();
       }
     }
   }
@@ -208,7 +208,7 @@ function Game() {
   }
 
   //Función que llena el tablero de celulas
-  function startGame(board) {
+  function startGame(obj) {
     let state;
 
     //Creamos la célula
@@ -217,20 +217,18 @@ function Game() {
         //Creamos un estado para la celula aleatorio
         state = Math.floor(Math.random() * 2);
         //Creamos un nuevo agente y le pasamos sus cordenadas y su condición
-        board[y][x] = new Cells(x, y, state);
+        obj[y][x] = new Cells(x, y, state);
       }
     }
 
     //Le añadimos sus vecinos
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < columns; x++) {
-        board[y][x].addNeighbours();
+        obj[y][x].addNeighbours();
       }
     }
   }
   // ================================
- 
-
   return (
     <div>
       <div className={styles.nav}>
